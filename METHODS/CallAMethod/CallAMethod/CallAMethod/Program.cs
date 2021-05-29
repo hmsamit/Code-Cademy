df584b49b979ff6311816ba728841b588180ac39
hmsamit <amithzlmae@gmail.com>
2021-05-29 18:36:58 +0800

    CaptureOutput_Methods

---
+++ METHODS/CallAMethod/CallAMethod/CallAMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CallAMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Call Math.Min() with two arguments (any two integers will do). You can view the documentation if you’re not sure how to use this method.
+            string msg = "Yabba dabba doo!";
+            Math.Min(16, 20);
+            //Call Console.WriteLine() using the variable msg as an argument.
+            Console.WriteLine(msg);
+            //Get the first letter of the msg string using Substring().
+            msg.Substring(0,1);
+
+
+        }
+    }
+}
---
+++ METHODS/CallAMethod/CaptureOutput/CaptureOutput/CaptureOutput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CaptureOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The designer of C# is "Anders Hejlsberg". His first name is nice, but we want to print the second name alone.
+
+           // First, find the index of the space(" ") in the string designer and store it in a variable indexOfSpace.
+
+            string designer = "Anders Hejlsberg";
+            //   // First, find the index of the space(" ") in the string designer and store it in a variable indexOfSpace.
+            int indexOfSpace = designer.IndexOf("");
+            //Use Substring() and the variable indexOfSpace to get the second name. Store the returned value in a variable secondName.
+            string secondName = designer.Substring(indexOfSpace);
+            //Print secondName to the console.
+            Console.WriteLine(secondName);
+        }
+    }
+}
