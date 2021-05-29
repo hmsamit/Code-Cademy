4b73fac96fa058f9b3395ac0129d383abe91c292
hmsamit <amithzlmae@gmail.com>
2021-05-29 18:58:20 +0800

    DefineAMethod

---
+++ METHODS/DefineAMethod/DefineAMethod/DefineAMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DefineAMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
