39e7592dd6a0cdaf68a7375d2f8a2b4ef2efd9b1
hmsamit <amithzlmae@gmail.com>
2021-05-30 14:32:33 +0800

    Named Arguments

---
+++ METHODS/NamedArguments/NamedArguments/NamedArguments/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NamedArguments
+{
+    //The VisitPlanets() method has some new optional parameters.
+    //First, call the method in Main() with no arguments.
+
+    //Call the method again, but define only the numberOfPlanets parameter as 2.
+
+    //Call the method one more time, now defining the numberOfPlanets as 2 and name with your name.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VisitPlanets(name: "Cosmonaut", numberOfPlanets: 2);;
+        }
+
+        static void VisitPlanets(
+          string adjective = "brave",
+          string name = "Cosmonaut",
+          int numberOfPlanets = 0,
+          double gForce = 4.2)
+        {
+            Console.WriteLine($"Welcome back, {adjective} {name}.");
+            Console.WriteLine($"You visited {numberOfPlanets} new planets...");
+            Console.WriteLine($"...while experiencing a g-force of {gForce} g!");
+            Console.ReadKey();  
+        }
+       
+    }
+}
