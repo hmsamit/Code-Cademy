72a40827013296298608b46f37a6569fbd0ac690
hmsamit <amithzlmae@gmail.com>
2021-07-12 11:53:13 +0800

    Boolean data types

---
+++ DATA TYPES AND VARIABLES/Working with text/VariablesReview_FINAL PART/VariablesReview/VariablesReview/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* use this space to write your own short program! 
+            Here's what you learned:
+
+            DATA TYPES: char, string
+            STRING INTERPOLATION: $"blah blah"
+            STRING INFO: .Length, .IndexOf()
+            PARTS OF STRINGS: bracketNotation[], .Substring() 
+            STRING MANIPULATION: .ToUpper(), .ToLower()
+            
+           --Now use what you’ve learned to write a short program! Some ideas:
+
+            1.) Write a program that randomly converts part of a text to uppercase and lowercase to      look like randomcase.
+            2.) Write a program that takes in a series of random words to construct an automated poem, in the style of e.e. cummings.
+            Good luck! */
+
+           
+        }
+    }
+}
