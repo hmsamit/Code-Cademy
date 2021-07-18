7de6c36c13f47d855425634adcb27dd10b9bfcb3
hmsamit <amithzlmae@gmail.com>
2021-07-18 15:38:34 +0800

    Money maker

---
+++ DATA TYPES AND VARIABLES/Working with text/MoneyMakerActivity/MoneyMaker/MoneyMaker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("Enter an amount to convert to coins: ");
+            string totalAsString = Console.ReadLine();
+            double totalAsDouble = Convert.ToDouble(totalAsString);
+            Console.WriteLine($"{totalAsDouble} cents is equal to...");
+            int gold = 10;
+            int silver = 5;
+
+            // calculating gold coins
+            double goldCoins = Math.Floor(totalAsDouble / gold);
+            double leftOver = totalAsDouble % gold;
+            double silverCoins = Math.Floor(leftOver / silver);
+            double remainder = leftOver % silver;
+
+            // printing all coins
+            Console.WriteLine($"Gold coins: {goldCoins}");
+            Console.WriteLine($"Silver coins: {silverCoins}");
+            Console.WriteLine($"Bronze coins: {remainder}");
+            Console.ReadKey();
+        }
+    }
+}
